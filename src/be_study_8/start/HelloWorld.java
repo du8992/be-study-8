5b8c5df1f3472a0525e0fabc182526d292d1052f
du8992 <ddongwook8992@gmail.com>
2026-05-06 13:51:41 +0900

    test

---
+++ src/be_study_8/start/HelloWorld.java
@@ -0,0 +1,11 @@
+package be_study_8.start;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+			System.out.println("hello, world!");
+			
+			System.out.println();
+	}
+
+}
