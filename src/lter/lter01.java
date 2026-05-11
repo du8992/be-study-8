496df0c34db0e8bfd668358124f0e19a83fb0788
du8992 <ddongwook8992@gamil.com>
2026-05-11 16:26:11 +0900

    for문

---
+++ src/lter/lter01.java
@@ -0,0 +1,28 @@
+package lter;
+
+public class lter01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("배고파");
+		for(int i=1; i<=10; i++) {
+			System.out.println("hungry");
+		}
+		System.out.println("end");
+		
+		int i = 0;
+		
+		for(i= 1; i < 3; i++) {
+			System.out.println("졸려");
+		}
+		System.out.println("end");
+		
+		for(i = 35; i < 45; ++i ) { 
+			System.out.println("이제 그만..");
+		}
+		System.out.println("end");
+		
+		System.out.println(i);
+	}
+
+}
